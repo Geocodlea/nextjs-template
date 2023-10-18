ee5de42466da49d5a134f822e54372b26af17126
Geocodlea <geocodlea@yahoo.com>
2023-10-18 23:14:55 +0300

    CRUD table users

--- app/admin/Users.js
+++ app/admin/Users.js
@@ -1,16 +1,239 @@
-import dbConnect from "/utils/dbConnect";
-import User from "/models/User";
+"use client";
 
-const Users = async () => {
-  await dbConnect();
-  const users = await User.find();
+import { useState, useCallback } from "react";
+
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
+import {
+  GridRowModes,
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarQuickFilter,
+  GridActionsCellItem,
+  GridRowEditStopReasons,
+} from "@mui/x-data-grid";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+const useFakeMutation = () => {
+  return useCallback(
+    (user) =>
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (user.name?.trim() === "") {
+            reject(new Error("Error while saving user: name can't be empty."));
+          } else {
+            resolve({ ...user, name: user.name?.toUpperCase() });
+          }
+        }, 200);
+      }),
+    []
+  );
+};
+
+function EditToolbar(props) {
+  const { rows, setRows, setRowModesModel } = props;
+
+  const handleClick = () => {
+    let id;
+    if (rows.length === 0) {
+      id = 1;
+    } else {
+      const lastId = rows[rows.length - 1].id;
+      id = lastId + 1;
+    }
+
+    setRows((oldRows) => [...oldRows, { id, name: "", age: "", isNew: true }]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
+    }));
+  };
 
   return (
-    <>
-      {users.map((user) => (
-        <p key={user.id}>{user.email}</p>
-      ))}
-    </>
+    <GridToolbarContainer
+      sx={{ display: "flex", justifyContent: "space-between" }}
+    >
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add record
+      </Button>
+      <GridToolbarQuickFilter />
+    </GridToolbarContainer>
+  );
+}
+
+const Users = ({ users }) => {
+  const usersArray = JSON.parse(users);
+
+  const initialRows = usersArray.map((user, index) => ({
+    id: index,
+    col1: user.name,
+    col2: user.email,
+    col3: user.role,
+  }));
+
+  const columns = [
+    { field: "col1", headerName: "Name", width: 200, editable: true },
+    { field: "col2", headerName: "Email", width: 200, editable: true },
+    { field: "col3", headerName: "Role", width: 200, editable: true },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              color="primary"
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="error"
+          />,
+        ];
+      },
+    },
+  ];
+
+  const [rows, setRows] = useState(initialRows);
+  const [rowModesModel, setRowModesModel] = useState({});
+
+  const handleRowEditStop = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const mutateRow = useFakeMutation();
+
+  const [snackbar, setSnackbar] = useState(null);
+
+  const handleCloseSnackbar = () => setSnackbar(null);
+
+  // const processRowUpdate = useCallback(
+  //   async (newRow) => {
+  //     // Make the HTTP request to save in the backend
+  //     const response = await mutateRow(newRow);
+  //     setSnackbar({ children: "User successfully saved", severity: "success" });
+  //     return response;
+  //   },
+  //   [mutateRow]
+  // );
+
+  const handleProcessRowUpdateError = useCallback((error) => {
+    setSnackbar({ children: error.message, severity: "error" });
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        height: 500,
+        width: "100%",
+        "& .actions": {
+          color: "text.secondary",
+        },
+        "& .textPrimary": {
+          color: "text.primary",
+        },
+      }}
+    >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        disableColumnFilter
+        disableColumnSelector
+        disableDensitySelector
+        processRowUpdate={processRowUpdate}
+        onProcessRowUpdateError={handleProcessRowUpdateError}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        slots={{ toolbar: EditToolbar }}
+        slotProps={{
+          toolbar: {
+            rows,
+            setRows,
+            setRowModesModel,
+          },
+        }}
+      />
+      {!!snackbar && (
+        <Snackbar
+          open
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          onClose={handleCloseSnackbar}
+          autoHideDuration={3000}
+        >
+          <Alert {...snackbar} onClose={handleCloseSnackbar} />
+        </Snackbar>
+      )}
+    </Box>
   );
 };
 
--- app/admin/page.js
+++ app/admin/page.js
@@ -7,11 +7,17 @@ import { Paper, Typography } from "@mui/material";
 import CreateEventForm from "./CreateEventForm";
 import Users from "./Users";
 
+import dbConnect from "/utils/dbConnect";
+import User from "/models/User";
+
 export default async function Admin() {
   const session = await getServerSession(authOptions);
 
   if (session?.user.role !== "admin")
     return <h1 className="text-5xl">NOT ALLOWED !!!</h1>;
+
+  await dbConnect();
+  const users = await User.find();
 
   return (
     <>
@@ -23,11 +29,9 @@ export default async function Admin() {
         <Typography variant="h2">Create Event</Typography>
         <CreateEventForm />
       </Paper>
-      <Paper elevation={24} className={styles.card}>
+      <Paper elevation={24} className={styles.card} sx={{ width: "100%" }}>
         <Typography variant="h2">Users</Typography>
-        <div>
-          <Users />
-        </div>
+        <Users users={JSON.stringify(users)} />
       </Paper>
     </>
   );
