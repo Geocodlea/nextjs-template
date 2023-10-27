0f08a1237976e9cb6dd45d70dcfdc75448ddc846
Geocodlea <geocodlea@yahoo.com>
2023-10-27 20:42:05 +0300

    about + loading page

---
+++ app/about/loading.js
@@ -0,0 +1,34 @@
+import styles from "../page.module.css";
+
+import { Box, Stack, Skeleton } from "@mui/material";
+
+const skeletons = [1, 2, 3, 4];
+
+export default function Loading() {
+  return (
+    <>
+      <Skeleton width="70%" className={styles.title} />
+      <Skeleton variant="rounded" width="100%" height="30vh" />
+      <Skeleton
+        variant="rounded"
+        width={200}
+        height={30}
+        sx={{ margin: "2rem 0" }}
+      />
+      <Box className={styles.grid}>
+        {skeletons.map((skeleton, index) => (
+          <Stack
+            spacing={1.5}
+            sx={{ margin: "20px", alignItems: "center" }}
+            key={index}
+          >
+            <Skeleton variant="circular" width={80} height={80} />
+            <Skeleton variant="rounded" width={85} height={32} />
+            <Skeleton variant="rounded" width={100} height={20} />
+            <Skeleton variant="rounded" width="100%" height={80} />
+          </Stack>
+        ))}
+      </Box>
+    </>
+  );
+}
--- app/about/page.js
+++ app/about/page.js
@@ -1,96 +1,76 @@
 import styles from "../page.module.css";
 
-export default function About() {
+import { Box, Typography, Stack, Avatar } from "@mui/material";
+
+export default async function Home({ searchParams }) {
   return (
-    <main className={styles.code}>
-      What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and
-      typesetting industry. Lorem Ipsum has been the industry's standard dummy
-      text ever since the 1500s, when an unknown printer took a galley of type
-      and scrambled it to make a type specimen book. It has survived not only
-      five centuries, but also the leap into electronic typesetting, remaining
-      essentially unchanged. It was popularised in the 1960s with the release of
-      Letraset sheets containing Lorem Ipsum passages, and more recently with
-      desktop publishing software like Aldus PageMaker including versions of
-      Lorem Ipsum. Why do we use it? It is a long established fact that a reader
-      will be distracted by the readable content of a page when looking at its
-      layout. The point of using Lorem Ipsum is that it has a more-or-less
-      normal distribution of letters, as opposed to using 'Content here, content
-      here', making it look like readable English. Many desktop publishing
-      packages and web page editors now use Lorem Ipsum as their default model
-      text, and a search for 'lorem ipsum' will uncover many web sites still in
-      their infancy. Various versions have evolved over the years, sometimes by
-      accident, sometimes on purpose (injected humour and the like). Where does
-      it come from? Contrary to popular belief, Lorem Ipsum is not simply random
-      text. It has roots in a piece of classical Latin literature from 45 BC,
-      making it over 2000 years old. Richard McClintock, a Latin professor at
-      Hampden-Sydney College in Virginia, looked up one of the more obscure
-      Latin words, consectetur, from a Lorem Ipsum passage, and going through
-      the cites of the word in classical literature, discovered the undoubtable
-      source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-      Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in
-      45 BC. This book is a treatise on the theory of ethics, very popular
-      during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor
-      sit amet..", comes from a line in section 1.10.32. The standard chunk of
-      Lorem Ipsum used since the 1500s is reproduced below for those interested.
-      Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
-      Cicero are also reproduced in their exact original form, accompanied by
-      English versions from the 1914 translation by H. Rackham. Where can I get
-      some? There are many variations of passages of Lorem Ipsum available, but
-      the majority have suffered alteration in some form, by injected humour, or
-      randomised words which don't look even slightly believable. If you are
-      going to use a passage of Lorem Ipsum, you need to be sure there isn't
-      anything embarrassing hidden in the middle of text. All the Lorem Ipsum
-      generators on the Internet tend to repeat predefined chunks as necessary,
-      making this the first true generator on the Internet. It uses a dictionary
-      of over 200 Latin words, combined with a handful of model sentence
-      structures, to generate Lorem Ipsum which looks reasonable. The generated
-      Lorem Ipsum is therefore always free from repetition, injected humour, or
-      non-characteristic words etc. 5 paragraphs words bytes lists Start with
-      'Lorem ipsum dolor sit amet...' What is Lorem Ipsum? Lorem Ipsum is simply
-      dummy text of the printing and typesetting industry. Lorem Ipsum has been
-      the industry's standard dummy text ever since the 1500s, when an unknown
-      printer took a galley of type and scrambled it to make a type specimen
-      book. It has survived not only five centuries, but also the leap into
-      electronic typesetting, remaining essentially unchanged. It was
-      popularised in the 1960s with the release of Letraset sheets containing
-      Lorem Ipsum passages, and more recently with desktop publishing software
-      like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it?
-      It is a long established fact that a reader will be distracted by the
-      readable content of a page when looking at its layout. The point of using
-      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-      as opposed to using 'Content here, content here', making it look like
-      readable English. Many desktop publishing packages and web page editors
-      now use Lorem Ipsum as their default model text, and a search for 'lorem
-      ipsum' will uncover many web sites still in their infancy. Various
-      versions have evolved over the years, sometimes by accident, sometimes on
-      purpose (injected humour and the like). Where does it come from? Contrary
-      to popular belief, Lorem Ipsum is not simply random text. It has roots in
-      a piece of classical Latin literature from 45 BC, making it over 2000
-      years old. Richard McClintock, a Latin professor at Hampden-Sydney College
-      in Virginia, looked up one of the more obscure Latin words, consectetur,
-      from a Lorem Ipsum passage, and going through the cites of the word in
-      classical literature, discovered the undoubtable source. Lorem Ipsum comes
-      from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
-      Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
-      treatise on the theory of ethics, very popular during the Renaissance. The
-      first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a
-      line in section 1.10.32. The standard chunk of Lorem Ipsum used since the
-      1500s is reproduced below for those interested. Sections 1.10.32 and
-      1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced
-      in their exact original form, accompanied by English versions from the
-      1914 translation by H. Rackham. Where can I get some? There are many
-      variations of passages of Lorem Ipsum available, but the majority have
-      suffered alteration in some form, by injected humour, or randomised words
-      which don't look even slightly believable. If you are going to use a
-      passage of Lorem Ipsum, you need to be sure there isn't anything
-      embarrassing hidden in the middle of text. All the Lorem Ipsum generators
-      on the Internet tend to repeat predefined chunks as necessary, making this
-      the first true generator on the Internet. It uses a dictionary of over 200
-      Latin words, combined with a handful of model sentence structures, to
-      generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is
-      therefore always free from repetition, injected humour, or
-      non-characteristic words etc. 5 paragraphs words bytes lists Start with
-      'Lorem ipsum dolor sit amet...'
-    </main>
+    <>
+      <Typography variant="h1" className={styles.title}>
+        About Us
+      </Typography>
+      <Typography className={styles.description} mb={3}>
+        We're passionate about helping you bring your creative ideas to life. We
+        understand the power of visuals and how they can transform your design
+        concepts into reality. Our mission is to provide you with the most
+        user-friendly, versatile, and innovative mockup tools on the web.
+      </Typography>
+      <Typography variant="h6" className={styles.code} mb={3}>
+        Here is our team:
+      </Typography>
+      <Box className={styles.grid} sx={{ textAlign: "center" }}>
+        <Stack spacing={1} sx={{ margin: "20px", alignItems: "center" }}>
+          <Avatar
+            alt="avatar"
+            src="/img/pers1.jpeg"
+            sx={{ width: 80, height: 80 }}
+          />
+          <Typography variant="h6">Elon Tusk</Typography>
+          <Typography variant="overline">CEO / Founder</Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+        </Stack>
+        <Stack spacing={1} sx={{ margin: "20px", alignItems: "center" }}>
+          <Avatar
+            alt="avatar"
+            src="/img/pers2.jpeg"
+            sx={{ width: 80, height: 80 }}
+          />
+          <Typography variant="h6">Rita Cora</Typography>
+          <Typography variant="overline">Web Engineer</Typography>
+          <Typography>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </Typography>
+        </Stack>
+        <Stack spacing={1} sx={{ margin: "20px", alignItems: "center" }}>
+          <Avatar
+            alt="avatar"
+            src="/img/pers3.jpeg"
+            sx={{ width: 80, height: 80 }}
+          />
+          <Typography variant="h6">Mark Berg</Typography>
+          <Typography variant="overline">Web Developer</Typography>
+          <Typography>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </Typography>
+        </Stack>
+        <Stack spacing={1} sx={{ margin: "20px", alignItems: "center" }}>
+          <Avatar
+            alt="avatar"
+            src="/img/pers4.jpeg"
+            sx={{ width: 80, height: 80 }}
+          />
+          <Typography variant="h6">Angelina Molie</Typography>
+          <Typography variant="overline">Web Designer</Typography>
+          <Typography>
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </Typography>
+        </Stack>
+      </Box>
+    </>
   );
 }
