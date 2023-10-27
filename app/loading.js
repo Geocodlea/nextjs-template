ba5d94831d1e92113f0b2c3dfc05667aedc79ef5
Geocodlea <geocodlea@yahoo.com>
2023-10-27 13:16:21 +0300

    homepage content

--- app/loading.js
+++ app/loading.js
@@ -6,25 +6,35 @@ const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Loading() {
   return (
-    <Box className={styles.grid}>
-      {skeletons.map((skeleton, index) => (
-        <Paper elevation={24} key={index} style={{ margin: "10px" }}>
-          <Skeleton variant="rounded" animation="wave" height={300} />
-          <Skeleton
-            variant="rectangular"
-            height={100}
-            sx={{ bgcolor: "grey.200" }}
-          />
-          <Skeleton
-            variant="text"
-            sx={{ fontSize: "2rem", marginLeft: "20%", marginRight: "20%" }}
-          />
-          <Skeleton
-            variant="text"
-            sx={{ fontSize: "2rem", marginLeft: "30%", marginRight: "30%" }}
-          />
-        </Paper>
-      ))}
-    </Box>
+    <>
+      <Skeleton width="80%" className={styles.title} />
+      <Skeleton variant="rounded" width="100%" height="30vh" />
+      <Skeleton
+        variant="rounded"
+        width="80%"
+        height="30px"
+        sx={{ margin: "2rem 0" }}
+      />
+      <Box className={styles.grid}>
+        {skeletons.map((skeleton, index) => (
+          <Paper elevation={24} key={index} style={{ margin: "10px" }}>
+            <Skeleton variant="rounded" animation="wave" height={300} />
+            <Skeleton
+              variant="rectangular"
+              height={100}
+              sx={{ bgcolor: "grey.200" }}
+            />
+            <Skeleton
+              variant="text"
+              sx={{ fontSize: "2rem", marginLeft: "20%", marginRight: "20%" }}
+            />
+            <Skeleton
+              variant="text"
+              sx={{ fontSize: "2rem", marginLeft: "30%", marginRight: "30%" }}
+            />
+          </Paper>
+        ))}
+      </Box>
+    </>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,27 @@
+import styles from "./page.module.css";
+
+import { Typography } from "@mui/material";
+
 import Events from "@/components/Events";
 
 export default async function Home({ searchParams }) {
-  return <Events searchParams={searchParams} />;
+  return (
+    <>
+      <Typography variant="h1" className={styles.title}>
+        Next Template
+      </Typography>
+      <Typography className={styles.description} mb={3}>
+        This is a Next.js boilerplate template that provides a solid foundation
+        for building modern web applications. It includes a well-structured
+        Next.js App Router folder structure, built using the latest technologies
+        and best practices. This boilerplate is preconfigured with NextAuth.js
+        for authentication. It integrates with MongoDB as the database and
+        supports multiple authentication providers.
+      </Typography>
+      <Typography variant="h6" className={styles.code} mb={3}>
+        The below cards are a Full CRUD example for an admin user:
+      </Typography>
+      <Events searchParams={searchParams} />
+    </>
+  );
 }
