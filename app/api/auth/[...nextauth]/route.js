351b2a7610dd143b33a795b75a517ed7def361d9
Geocodlea <geocodlea@yahoo.com>
2023-11-09 23:01:23 +0200

    vercel test 3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -68,7 +68,7 @@ export const authOptions = {
   },
 
   pages: {
-    signIn: "/auth/signin",
+    //signIn: "/auth/signin",
     // signOut: "/auth/signout",
     error: "/auth/error", // Error code passed in query string as ?error=
     verifyRequest: "/auth/verify-request", // (used for check email message)
