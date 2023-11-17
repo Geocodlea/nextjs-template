d7a73a1c6da7809da54570a86d962df2964984d0
Geocodlea <geocodlea@yahoo.com>
2023-11-17 12:39:58 +0200

    netlify 14

--- decode-cloud-storage-key.mjs
+++ decode-cloud-storage-key.mjs
@@ -4,4 +4,4 @@ const base64EncodedString = process.env.GOOGLE_CLOUD_KEY_FILE_ENCODED;
 const decodedBuffer = Buffer.from(base64EncodedString, "base64");
 const decodedString = decodedBuffer.toString("utf-8");
 
-writeFileSync("secrets/google-cloud-key.json", decodedString);
+writeFileSync("test.json", decodedString);
