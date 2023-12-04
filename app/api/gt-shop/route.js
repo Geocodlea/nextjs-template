4ac2fcdb6e87ec4781ceee9109d08a989bbc12c7
Geocodlea <geocodlea@yahoo.com>
2023-12-04 15:33:46 +0200

    5

--- app/api/gt-shop/route.js
+++ app/api/gt-shop/route.js
@@ -1,18 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function OPTIONS(request) {
-  // Respond to pre-flight requests
-  return NextResponse.json({
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
-    },
-    body: "",
-  });
-}
-
 export async function POST(request) {
   const data = await request.json();
 
@@ -20,8 +7,7 @@ export async function POST(request) {
 
   try {
     const response = await fetch(
-      "https://api.xperiencify.io/api/public/student/customfield/?api_key=" +
-        process.env.GT_SHOP_API_KEY,
+      `https://api.xperiencify.io/api/public/student/customfield/?api_key=${process.env.GT_SHOP_API_KEY}`,
       {
         method: "POST",
         headers: {
